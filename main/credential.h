fec99e2e0c7ff20eb979f78e9023bf1e15ccc0d5
Elouan Lerissel <elouan.lerissel@gmail.com>
2022-05-24 17:25:46 +0200

    fixed untracked files

--- main/credential.h
+++
@@ -1,5 +0,0 @@
-//const char* ssid       PROGMEM= "Livebox-6858";
-//const char* password   PROGMEM= "xVdxfd4CVHGTHD82Hx";
-
-const char* ssid       PROGMEM= "Elouan's iPhone";
-const char* password   PROGMEM= "le moulin";
